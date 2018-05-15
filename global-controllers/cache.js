5a2f641eada7493ae88d466c9b257738a4fc3410
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2018-05-15 08:40:13 +0300

    fix

--- global-controllers/cache.js
+++ global-controllers/cache.js
@@ -2,7 +2,7 @@ module.exports = (cacheService, loggerService) => async (req, res, next) => {
     const cached = await cacheService.get(req);
 
     if (cached) {
-        loggerService.log(req, 'FROM CACHE');
+        //loggerService.log(req, 'FROM CACHE');
         res.json(cached);
         return;
     }
